dbd06e4905f28b62742e5587fadebdb62dfaf149
keerti kumari <kirtichaudhary492@gmail.com>
2026-06-28 15:26:39 +0530

    Add Basic Chatbot project

---
+++ chatbot.py
@@ -0,0 +1,19 @@
+def chatbot():
+
+    print("welcome to basic chatbot!")
+    print("typ bye to exit.")
+
+    while True:
+
+        user = input("you:")
+        if user == "hello":
+            print("Bot.hi!")
+        elif user == "how are you":
+                print("Bot.i am fine, thanks!")
+        elif user == "bye":
+                    print("Bot: goodbye!")
+                    break
+        else:
+            print("Bot:sorry,i don't understand.")
+chatbot()
+
